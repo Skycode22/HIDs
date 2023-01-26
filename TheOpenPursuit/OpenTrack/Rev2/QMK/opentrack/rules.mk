85a968a92d2fcf458bdae267c6f300eaf624ed62
Skycode22 <97566685+Skycode22@users.noreply.github.com>
2023-01-25 22:39:48 -0800

    added encoder support

--- TheOpenPursuit/OpenTrack/Rev2/QMK/opentrack/rules.mk
+++ TheOpenPursuit/OpenTrack/Rev2/QMK/opentrack/rules.mk
@@ -17,6 +17,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+ENCODER_ENABLE = yes
 
 # Add trackball support
 POINTING_DEVICE_ENABLE = yes
