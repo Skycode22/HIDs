be7c0a8952b62007ce71b6f8cc276aaae0674ba9
Skycode22 <97566685+Skycode22@users.noreply.github.com>
2023-04-16 20:58:50 -0700

    trashtruck V2 added and 3d files added

---
+++ TrashTruck/V2/qmk/trashtruckv2/keymaps/default/keymap.c
@@ -0,0 +1,17 @@
+// Copyright 2023 %YOUR_FULL_NAME% (@%YOUR_GITHUB_USERNAME%)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, XXXXXXX, KC_HOME, XXXXXXX, KC_PGUP,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,  XXXXXXX, KC_5,    XXXXXXX,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, KC_END,  XXXXXXX, KC_PGDN, XXXXXXX, KC_LCTL, KC_LALT, KC_SPC,  KC_LALT, KC_LCTL, KC_INS,  KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+    [1] = LAYOUT(
+        QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    )
+};
