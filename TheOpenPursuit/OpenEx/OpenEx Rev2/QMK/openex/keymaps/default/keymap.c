aa5841f28a8e8b507ed2ffb33a196fa1cd6a3245
Skycode22 <97566685+Skycode22@users.noreply.github.com>
2023-05-29 15:42:34 -0700

    front page

---
+++ TheOpenPursuit/OpenEx/OpenEx Rev2/QMK/openex/keymaps/default/keymap.c
@@ -0,0 +1,27 @@
+// Copyright 2022 %YOUR_FULL_NAME% (@%YOUR_GITHUB_USERNAME%)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_TAB,  KC_BTN1,  KC_BTN2,  KC_LSFT   
+    ),
+
+    [1] = LAYOUT(
+        RESET,   _______, _______, _______  
+    ),
+
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_WH_U, 10);
+        } else {
+            tap_code_delay(KC_WH_D, 10);
+        }
+    }
+    return false;
+}
